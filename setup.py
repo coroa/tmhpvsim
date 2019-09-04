747cff9acb619b2bde44ae3a70d0330cad4a28c0
Jonas Hoersch <coroa@posteo.de>
2019-09-04 12:52:31 +0200

    initial framework

---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+import versioneer
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tmhpvsim",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Jonas Hörsch",
+    author_email="jonas.hoersch@posteo.de",
+    description="Simple PV simulation and network streaming",
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url="https://github.com/coroa/tmhpvsim",
+    packages=setuptools.find_packages(exclude=['docs', 'test']),
+    include_package_data=True,
+    install_requires=[
+        pvlib,
+        tables,
+        matplotlib,
+        xarray,
+        netcdf4,
+        pymc3,
+        siphon,
+        pika,
+        arviz,
+        seaborn,
+        cdsapi
+    ],
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
